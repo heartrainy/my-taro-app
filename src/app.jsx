2821ca75eaf1867f27f427b7545c01c77659f0ec
heartrainy <526132448@qq.com>
2019-09-09 17:44:34 +0800

    提交

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
+import { set as setGlobalData, get as getGlobalData } from './global_data'
 import Index from './pages/index'
 
 import './app.scss'
@@ -22,6 +23,48 @@ class App extends Component {
       navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
     }
+  }
+
+  componentWillMount () {
+    // 展示本地存储能力
+    var logs = Taro.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    Taro.setStorageSync('logs', logs)
+
+    // 登录
+    Taro.login({
+      success: res => {
+        // console.log(res)
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+
+    // 获取用户信息
+    Taro.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          Taro.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              // this.globalData.userInfo = res.userInfo
+              // console.log(res)
+              setGlobalData('userInfo', res.userInfo);
+              // Taro.setStorageSync('avatar', res.userInfo.avatarUrl)
+              // Taro.setStorageSync('nickname', res.userInfo.nickName)
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.$app.userInfoReadyCallback) {
+                this.$app.userInfoReadyCallback(res)
+              }
+            }
+          })
+          // if (this.loginCallback) {
+          //   this.loginCallback(res)
+          // }
+        }
+      }
+    })
   }
 
   componentDidMount () {}
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,15 +1,57 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import { AtButton } from 'taro-ui'
+import { set as setGlobalData, get as getGlobalData } from '../../global_data'
 import './index.scss'
 
+const app = Taro.getApp()
+
 export default class Index extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      userInfo: {},
+      hasUserInfo: false,
+      canIUse: wx.canIUse('button.open-type.getUserInfo')
+    }
+  }
 
   config = {
     navigationBarTitleText: '首页'
   }
 
-  componentWillMount () { }
+  componentWillMount () { 
+    const userInfo = getGlobalData('userInfo')
+    if (userInfo) {
+      this.setState({
+        userInfo: userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.state.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setState({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      console.log(3)
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      Taro.getUserInfo({
+        success: res => {
+          // app.globalData.userInfo = res.userInfo
+          setGlobalData('userInfo', res.userInfo)
+          this.setState({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  }
 
   componentDidMount () { }
 
@@ -19,6 +61,15 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
+  getUserInfo = (e) => {
+    console.log(e)
+    setGlobalData('userInfo', e.detail.userInfo)
+    this.setState({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+
   goToPage = (url) => {
     Taro.navigateTo({
       url: `/pages/${url}/index`
@@ -26,10 +77,14 @@ export default class Index extends Component {
   }
 
   render () {
+    const { hasUserInfo, canIUse, userInfo } = this.state
     return (
       <View className='index'>
-        <Text>Hello world!!</Text>
-        <AtButton type='primary' onClick={() => this.goToPage('handwrite')}>跳转</AtButton>
+        <Text>Hello {userInfo.nickName}!!</Text>
+        <AtButton type='primary' onClick={() => this.goToPage('handwrite')}>手写板</AtButton>
+        {
+          !hasUserInfo && canIUse ? <Button openType="getUserInfo" onGetuserinfo={this.getUserInfo}>获取用户信息</Button> : ''
+        }
       </View>
     )
   }
